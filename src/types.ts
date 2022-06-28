1035a23a39577abbfb909d9dfd3e6157cbcc3493
Artemiy Schukin <shuckinart@yandex.ru>
2022-06-28 21:54:43 +0300

    Should be ok

--- src/types.ts
+++ src/types.ts
@@ -2,8 +2,10 @@ export type Thenable<T> = Promise<T> | T
 export type Hook = (value: string) => Thenable<string | void>
 export type BuildSvgDataURI = (html: Node, options: Options) => Promise<string>
 
+declare const array: string[]
+
 export interface Options {
-  filterer?: (value: string, index?: number, array?: string[]) => boolean
+  filterer?: Parameters<typeof array.filter>[0]
   fetcher?: (url: string) => Promise<string>
   css?: Hook
   width: number
